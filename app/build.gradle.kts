62083e54592e9e913570aa5e5011a0e2e87d6cd3
brnas02 <aluno23025@ipt.pt>
2024-11-29 18:05:58 +0100

    App Design
    Doing the app design for interation with the use

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.finalproject"
-    compileSdk = 34
+    compileSdk = 35 // Updated
 
     defaultConfig {
         applicationId = "com.example.finalproject"
